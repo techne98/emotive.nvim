5428cd02b94517e22c32899bc9816aec8f2f07df
techne98 <techne98@proton.me>
2026-02-10 17:31:25 +0400

    Move files into plugin style directory
    This starts creating a Neovim plugin style directory structure instead
    of just having random Lua files in the directory.
    
    I also added some minor renaming (e.g. emojis.lua -> emoji_db.lua)
    as I feel it is a little easier to understand.

--- lua/emotive/init.lua
+++ lua/emotive/init.lua
@@ -3,7 +3,7 @@ local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
-local emoji_db = require("emojis")
+local emoji_db = require("emoji_db")
 
 local function get_formatted_emojis()
     local results = {}
