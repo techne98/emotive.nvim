91bce759d648a8fb54897b2de3829c95db8dc285
techne98 <techne98@proton.me>
2026-02-16 09:58:20 +0400

    Refactor to use Entry Maker
    I guess I didn't need to build my own cache, as by using Entry Maker
    it forgoes the need to use a custom `get_formatted_emojis()` function
    entirely.
    
    From what I understand, Entry Maker works lazily so there may be some
    performance benefits to this approach. It also seems more idiomatic,
    and although I'm still concatenating the string for the display key,
    I can now select the emoji using `selection.value.emoji` which seems
    like a better approach.
    
    This change doesn't affect functionality otherwise.

--- lua/emotive/init.lua
+++ lua/emotive/init.lua
@@ -5,38 +5,27 @@ local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
 local emoji_db = require("emotive.emoji_db")
 
-local emoji_cache = nil
-
-local function get_formatted_emojis()
-    if emoji_cache then
-        vim.notify("Using cache, did not generate new table", vim.log.levels.INFO)
-        return emoji_cache
-    end
-
-    local results = {}
-    for _, emoji in ipairs(emoji_db) do
-        local emoji_str = emoji.emoji .. " " .. emoji.name
-        table.insert(results, emoji_str)
-    end
-    vim.notify("Emoji table created", vim.log.levels.INFO)
-    emoji_cache = results
-    return results
-end
-
 local emojis = function(opts)
   opts = opts or {}
   pickers.new(opts, {
     prompt_title = "search emojis",
     finder = finders.new_table {
-      results = get_formatted_emojis()
+      results = emoji_db,
+      entry_maker = function(entry)
+        local emoji_str = entry.emoji .. " " .. entry.name
+        return {
+          value = entry,
+          display = emoji_str,
+          ordinal = emoji_str
+        }
+      end,
     },
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
       actions.select_default:replace(function()
       actions.close(prompt_bufnr)
       local selection = action_state.get_selected_entry()
-      local emoji = selection[1]:match("^%S*")
-      vim.api.nvim_put({ emoji }, "", false, true)
+      vim.api.nvim_put({ selection.value.emoji }, "", false, true)
       end)
       return true
     end,
